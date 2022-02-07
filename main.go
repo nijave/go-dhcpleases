18e083218f80b76d2cc03881d1d3cc0e9a295eb8
Nick Venenga <nick@venenga.com>
2022-02-07 16:28:41 -0500

    Change pid tracking data structure

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"regexp"
 	"sort"
 	"strings"
-	"sync"
 	"syscall"
 	"time"
 )
--- pidfile.go
+++ pidfile.go
@@ -5,35 +5,45 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 )
 
 type DynamicPid struct {
-	lock    sync.Mutex
-	pid     int
-	PidFile string
+	pidAvailable chan int
+	pid          *int64
+	signals      chan syscall.Signal
+	PidFile      string
 }
 
 func (p DynamicPid) GetPid() int {
-	var pid int
-	p.lock.Lock()
-	pid = p.pid
-	p.lock.Unlock()
-	return pid
+	return int(atomic.LoadInt64(p.pid))
 }
 
-func (p DynamicPid) SetPid(pid int) {
-	p.lock.Lock()
-	p.pid = pid
-	p.lock.Unlock()
+func (p DynamicPid) setPid(pid int) {
+	atomic.StoreInt64(p.pid, int64(pid))
 }
 
-func (p DynamicPid) Watch() {
+func StartWatcher(pidFile string) *DynamicPid {
+	emptyPid := int64(-1)
+	watcher := &DynamicPid{
+		pidAvailable: make(chan int, 1),
+		pid:          &emptyPid,
+		signals:      make(chan syscall.Signal, 1),
+		PidFile:      pidFile,
+}
+
+	go watcher.watch()
+	go watcher.notifier()
+
+	return watcher
+}
+
+func (p DynamicPid) watch() {
 	data := make([]byte, 8)
 	pidFileWatch := KeventWatch{Filename: p.PidFile}
-	events := pidFileWatch.Watch(false)
+	fileWatchEvents := pidFileWatch.Watch(false)
 
 	for {
 		log.Printf("[pid] reading %s\n", p.PidFile)
@@ -60,23 +70,38 @@ func (p DynamicPid) Watch() {
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		p.SetPid(pid)
+		log.Printf("[pid] read pid=%d from %s\n", pid, p.PidFile)
+		p.setPid(pid)
+
+		p.pidAvailable <- pid
 
 		log.Printf("[pid] watching pid file %s for changes\n", p.PidFile)
-		<-events
+		<-fileWatchEvents
 	}
 }
 
-func (p DynamicPid) Notify(signal syscall.Signal) bool {
-	if p.GetPid() < 1 {
-		return false
+func (p DynamicPid) notifier() {
+	log.Println("[pid] notifier waiting on pid")
+	<-p.pidAvailable
+	log.Printf("[pid] notifier got pid=%d\n", p.GetPid())
+
+	if p.GetPid() == -1 {
+		log.Println("[pid] err pid is still -1")
+		return
 	}
 
+	var signal syscall.Signal
+	for {
+		signal = <-p.signals
+		log.Printf("[signal] send signum=%d pid=%d\n", signal, p.GetPid())
 	err := syscall.Kill(p.GetPid(), signal)
 	if err != nil {
-		log.Printf("[notify] %v notifying dnsmasq\n", err)
-		return false
+			log.Printf("[notify] %v notifying pid=%d\n", err, p.GetPid())
+		}
+	}
 	}
 
-	return true
+func (p DynamicPid) Notify(signal syscall.Signal) {
+	log.Printf("[signal] queue signum=%d pid=%d\n", signal, p.GetPid())
+	p.signals <- signal
 }
